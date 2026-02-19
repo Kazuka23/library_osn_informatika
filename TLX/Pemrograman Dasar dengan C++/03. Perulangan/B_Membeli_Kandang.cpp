cdf15d5ab7c68fa68d493ba42e445a640450542e
Kazuka23 <syabanmuhammad12@gmail.com>
2026-02-19 11:12:29 +0700

    Add B_Membeli_Kandang.cpp with basic operations

---
+++ TLX/Pemrograman Dasar dengan C++/03. Perulangan/B_Membeli_Kandang.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+}
