95e252b7ff1747fb474aa40ad6759f4ced916c78
Kazuka23 <syabanmuhammad12@gmail.com>
2026-02-19 11:13:17 +0700

    Add C++ program using while loop for kennel area

---
+++ TLX/Pemrograman Dasar dengan C++/03. Perulangan/C_Perkenalan_While.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+    int total_kandang = 0;
+
+    while ( total_kandang < 3 ) {
+        luas_kandang += 7;
+        cout << luas_kandang << endl;
+        
+        total_kandang += 1;
+
+
+    }
+}
