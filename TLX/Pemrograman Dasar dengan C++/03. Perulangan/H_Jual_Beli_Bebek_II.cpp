db18e315536ce14917a2f42160bc4906e6860bb9
Kazuka23 <syabanmuhammad12@gmail.com>
2026-02-19 11:21:35 +0700

    Add H_Jual_Beli_Bebek_II.cpp for duck selling simulation

---
+++ TLX/Pemrograman Dasar dengan C++/03. Perulangan/H_Jual_Beli_Bebek_II.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int jantan = 0, betina = 0;
+    int tanggal = 1;
+
+    while (betina <= 10 * jantan) {
+        jantan ++;
+        betina += tanggal;
+        tanggal = tanggal + 1;
+    }
+
+    cout << tanggal << endl;
+}
