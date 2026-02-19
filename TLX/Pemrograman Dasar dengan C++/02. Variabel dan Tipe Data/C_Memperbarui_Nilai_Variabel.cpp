8541dcb07f974c636fec771de9b11ada5129ab38
Kazuka23 <syabanmuhammad12@gmail.com>
2026-02-19 11:04:21 +0700

    Add C++ program to update variable values

---
+++ TLX/Pemrograman Dasar dengan C++/02. Variabel dan Tipe Data/C_Memperbarui_Nilai_Variabel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sandi = 174;
+    cout << sandi << endl;
+
+    sandi = 174*23;
+    cout << sandi << endl;
+
+    sandi = (174*23)*23;
+    cout << sandi << endl;
+}
