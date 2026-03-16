0d1b2e59c469bfbcd1dfae2fa4081d2f9ecf40b7
Kazuka23 <syabanmuhammad12@gmail.com>
2026-03-16 14:30:19 +0700

    Add C++ program for perfect squares with continue/break
    Implement a program to find perfect squares in a range.

---
+++ TLX/Pemrograman Dasar dengan C++/05. Perulangan & Percabangan/C_Perkenalan_Continue_dan_Break.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int luas = 100000; luas <= 200000; luas++) {
+        
+        if (luas % 2 != 0) {
+            continue;
+        }
+        
+        bool kuadrat_sempurna = false;
+
+        for (int k = 1; k <= luas; k++) {
+
+            if (luas == k*k) {
+                kuadrat_sempurna = true; break;
+            }
+        }
+
+        if (kuadrat_sempurna) {
+            cout << luas << endl;
+            break;
+        }
+    }
+}
